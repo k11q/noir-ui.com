4db77aded843d258e52b0450a6b9bbce3370872b
Khairul Haaziq <rhycoz@yahoo.com>
2023-01-12 14:23:51 +0800

    add dependancies

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<div>
+	<slot />
+</div>
+
+<style>
+	:global(body) {
+		background-color: #111111;
+		font-family: 'Clash Display', sans-serif;
+	}
+</style>
